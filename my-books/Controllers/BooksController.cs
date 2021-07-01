aa614ae9b2d97ff3f943b78c9ac0f5658a5adf2c
Jon-Dev21 <jonacevedev96@gmail.com>
2021-07-01 17:27:45 -0400

    Various Changes
    Used BooksController to define HttpEndpoints
    Created BookVM (Book View model) to define the book parameters that we want to pass to the BooksService class.
    Created BooksService file to define methods used by the BooksController
    Used BooksService methods to make http get, post, put and delete requests.
    Used Swagger to test out API.

--- my-books/Controllers/BooksController.cs
+++ my-books/Controllers/BooksController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using my_books.Data.Models.ViewModels;
+using my_books.Data.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +14,53 @@ namespace my_books.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        // Injecting BooksService to enable using booksService methods for http requests
+        public BooksService _booksService;
 
+        public BooksController(BooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        // Get Endpoint for returning all books
+        [HttpGet("get-all-books")]
+        public IActionResult GetAllBooks()
+        {
+            var allBooks = _booksService.GetAllBooks();
+            return Ok(allBooks);
+        }
+
+        // Get Endpoint for returning a single book by id
+        [HttpGet("get-book/{BookID}")]
+        public IActionResult GetBook(int BookID)
+        {
+            var book = _booksService.GetBookById(BookID);
+            return Ok(book);
+        }
+
+        // Creating Post endpoint
+        // For custom endpoint, add route between parenthesis
+        [HttpPost("add-book")]
+        public IActionResult Addbook([FromBody] BookVM book)
+        {
+            _booksService.AddBook(book);
+            return Ok();
+        }
+
+        // Book returned from request body
+        [HttpPut("update-book-by-id/{id}")]
+        public IActionResult UpdateBookById(int id, [FromBody]BookVM book)
+        {
+            var updatedBook = _booksService.UpdateBookById(id, book);
+            return Ok(updatedBook);
+        }
+
+        [HttpDelete("delete-book-by-id/{id}")]
+        public IActionResult DeleteBookById(int id)
+        {
+            _booksService.DeleteBookById(id);
+            return Ok();            
+        }
+        
     }
 }
---
+++ my-books/Data/Services/BooksService.cs
@@ -0,0 +1,77 @@
+﻿using my_books.Data.Models;
+using my_books.Data.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data.Services
+{
+    // This class is used to configure the Http services for the Books class
+    public class BooksService
+    {
+        // Database context variable
+        private AppDbContext _context;
+        public BooksService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Method used to add books to the database (POST)
+        public void AddBook(BookVM book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.IsRead ? book.Rate.Value : null,
+                Genre = book.Genre,
+                Author = book.Author, 
+                CoverURL = book.CoverURL,
+                DateAdded = DateTime.Now
+            };
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+
+        // Returns all books from the database
+        public List<Book> GetAllBooks() => _context.Books.ToList();
+    
+        // Returns a single book using specified ID
+        public Book GetBookById(int bookId) => _context.Books.FirstOrDefault(n => n.Id == bookId);
+
+        // Updates book using specified ID
+        public Book UpdateBookById(int bookId, BookVM book)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.Id == bookId);
+            // IF the book exists, update it.
+            if(_book != null)
+            {
+                _book.Title = book.Title;
+                _book.Description = book.Description;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.IsRead ? book.DateRead.Value : null;
+                _book.Rate = book.IsRead ? book.Rate.Value : null;
+                _book.Genre = book.Genre;
+                _book.Author = book.Author;
+                _book.CoverURL = book.CoverURL;
+
+                _context.SaveChanges();
+            };
+            return _book;
+        }
+
+        // Deletes book using specified ID
+        public void DeleteBookById(int bookId)
+        {
+            var _book = _context.Books.FirstOrDefault(n => n.Id == bookId);
+            if(_book != null)
+            {
+                _context.Books.Remove(_book);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
