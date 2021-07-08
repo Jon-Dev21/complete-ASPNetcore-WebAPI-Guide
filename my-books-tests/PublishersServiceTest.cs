471d7754d426118d387569db5321762cf7b9169c
Jon-Dev21 <jonacevedev96@gmail.com>
2021-07-08 17:49:49 -0400

    Will use NUnit for unit testing API
    Setting up Unit testing environment
    
    Creating new proyect my-books-test using NUnit Test project template.
    Will be used to test the PublishersService and PublishersController.
    
    In my-books-test dependencies, adding and installing new package
    microsoft.entityframeworkcore.inmemory
    
    this package is used to create an inMemory database used for unit testing.
    
    Added reference to my-books project in the my-books-test
    
    Next will be setting up Unit Testing In-Memory DB

---
+++ my-books-tests/PublishersServiceTest.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace my_books_tests
+{
+    public class PublishersServiceTest
+    {
+        // Setup Decorator
+        // This code runs each time a method is executed.
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+    }
+}
