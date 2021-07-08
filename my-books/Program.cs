86b70a7626849b9e2158b6c688733deaf52a5893
Jon-Dev21 <jonacevedev96@gmail.com>
2021-07-08 13:11:15 -0400

    Installed the following packages: - serilog.aspnetcore (serilog support for ASP.NET Core logging - serilog.sinks.File (Used to write serilog events to text files in plain JSON format) - serilog.sinks.mss (Serilog sink that writes events to Microsoft SQL Server)
    Configured serilog use in Program.cs
    
    using Serilog
    
    Created logger in main method. Added try, finally block. Close and flush log on finally block.

--- my-books/Program.cs
+++ my-books/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,11 +14,24 @@ namespace my_books
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            try
+            {
+                // Creating Logger for serilog
+                Log.Logger = new LoggerConfiguration().CreateLogger();
+
+                CreateHostBuilder(args).Build().Run();
+            } 
+            finally
+            {
+                // Resets logger to default and disposes original if possible
+                Log.CloseAndFlush();
+            }
+            
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()           // Instruction to enable use of serilog
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
